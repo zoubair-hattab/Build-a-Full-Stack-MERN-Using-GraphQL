59cfa205d79e3aec1ab8d6497bb001cd128731e3
zoubair-hattab <elhattabzoubair93@gmail.com>
2024-06-12 10:53:01 +0100

    fix bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: '/graphql',
 
   cache: new InMemoryCache(),
   credentials: 'include',
